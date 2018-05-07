56c42cbb6994138642353457335242e2c154e2a4
Rupam <rupam.shaw@gmail.com>
2018-05-08 09:12:12 +1000

    only video list comes from  Search

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import YTSearch from "youtube-api-search";
+import _ from "lodash";
+
+import VideoList from "./video_list";
+import SearchBar from './search_bar'
+
+const API_KEY= 'AIzaSyAEcEPSJPjC3AMtjxaeQ0SAyyd6V7huaUs'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      console.log('videos ',videos)
+      this.setState({
+        videos: videos
+      });
+    });
+  }
+
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
-      <div>React simple starter</div>
-    );
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoList
+          videos={this.state.videos}
+        />
+      </div>
+    )
   }
 }
